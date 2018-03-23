de9d49dca59cf7e437e9735e0c74d69452e67287
Vita <vita.petrenko.vp@gmail.com>
2018-03-23 16:05:10 +0200

    my first commit

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -5,7 +5,34 @@ import { Component } from '@angular/core';
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  constructor() {
+  bar:string;
+  items; 
 
+  constructor() {
+    this.initializeItems();
+  }
+
+  initializeItems() {
+    this.items = [
+      'Apple',
+      'Samsung',
+      'Huawei',
+      'Windows Phone' 
+    ];
+  }
+
+  getItems(ev) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the ev target
+    var val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
   }
 }
